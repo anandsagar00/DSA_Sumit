f24047292926dc58f8c3b290a847ab7f3398dc65
Anand Sagar <anandsagar152@gmail.com>
2023-01-25 13:07:21 +0530

    Update :Changed Logic in rotate number,handled negative rotation as well

--- rotate_number.cpp
+++ rotate_number.cpp
@@ -1,12 +1,20 @@
 // https://youtu.be/ysMHtDIlU0c?list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk
 
-//My This solution works for Input with non-zero digits and inputs which are +ve
-
-//Rotate a number by K digits
-
 /*
-27391 becomes 91 273 , after rotation when k=2
+1. You are given two numbers n and k. You are required to rotate n, k times to the right.
+If k is positive, rotate to the right i.e. remove rightmost digit and make it leftmost.
+Do the reverse for negative value of k. Also k can have an absolute value larger than number of digits in n.
+2. Take as input n and k.
+3. Print the rotated number.
+4. Note - Assume that the number of rotations will not cause leading 0's in the result. e.g. such an input will not be given
+   n = 12340056
+   k = 3
+   r = 05612340
+
+   ex: n = 273516 , k=2 , ans = 162735
 */
+
+//Solution : https://youtu.be/lt8oCGqYMGg?list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk
 
 #include<iostream>
 #include<math.h>
@@ -17,41 +25,30 @@ int main()
     int n,k;
     cin>>n>>k;
 
-    int left=0,right=0;
-
-    int copyN=n;
-
-    int pow1=1,pow2=1;
-
+    int cn=n;//copy of number
     int count_digits=0;
 
-    while(copyN>0)
+    while(n>0)
     {
         count_digits++;
-        copyN/=10;
+        n/=10;
     }
 
     k=k%count_digits;
-    int copyK=k;
-    copyN=n;
 
-    while(copyN>0)
-    {
-        if(copyK>0)
-        {
-            right=(pow1*(copyN%10))+right;
-            pow1*=10;
-        }
-        else
-        {
-            left=(pow2*(copyN%10))+left;
-            pow2*=10;
-        }
-        copyK--;
-        copyN/=10;
-    }
+    if(k<0)
+    k+=count_digits;
 
-    int ans=(right*pow(10,count_digits-k))+left;
+    int divisor=pow(10,k);
+
+    int multiplier=pow(10,count_digits-k);
+
+    int quotient=cn/divisor;
+    int rem=cn%divisor;
+
+    int first_part=multiplier*rem;
+
+    int ans=first_part+quotient;
 
     cout<<ans<<"\n";
 
