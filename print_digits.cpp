a87ce6358d112c174ea1f20d103bf1110b1bf956
Anand Sagar <anandsagar152@gmail.com>
2023-01-25 11:57:54 +0530

    Print digits from left to right

---
+++ print_digits.cpp
@@ -0,0 +1,33 @@
+// https://youtu.be/wq-Cp3kl1nI?list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk
+
+//Sol : https://youtu.be/gZp776a-pbQ?list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk
+
+//Print Digits of a number from left to right
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    if(n==0)
+    cout<<"0\n";
+
+    int temp=1;
+    int cn=n;//to keep a copy of the number
+
+    while(cn>=9)
+    {
+        temp*=10;
+        cn/=10;
+    }
+
+    while(temp>0)
+    {
+        cout<<n/temp<<"\n";
+        n=n%temp;
+        temp/=10;
+    }
+}
