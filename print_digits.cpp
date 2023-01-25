ca2c0b1a425ec3c4ee9d0972c28bfebc1e1533f7
Anand Sagar <anandsagar152@gmail.com>
2023-01-25 12:00:39 +0530

    Update : Print Digits from L-R of a number

--- print_digits.cpp
+++ print_digits.cpp
@@ -1,33 +1,35 @@
 // https://youtu.be/wq-Cp3kl1nI?list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk
 
-//Sol : https://youtu.be/gZp776a-pbQ?list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk
+// Sol : https://youtu.be/gZp776a-pbQ?list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk
 
-//Print Digits of a number from left to right
+// Print Digits of a number from left to right
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
 
-    if(n==0)
-    cout<<"0\n";
-
-    int temp=1;
-    int cn=n;//to keep a copy of the number
-
-    while(cn>=9)
+    if (n <= 9)
+        cout << n << "\n";
+    else
     {
-        temp*=10;
-        cn/=10;
-    }
+        int temp = 1;
+        int cn = n; // to keep a copy of the number
 
-    while(temp>0)
-    {
-        cout<<n/temp<<"\n";
-        n=n%temp;
-        temp/=10;
+        while (cn >= 9)
+        {
+            temp *= 10;
+            cn /= 10;
+        }
+
+        while (temp > 0)
+        {
+            cout << n / temp << "\n";
+            n = n % temp;
+            temp /= 10;
+        }
     }
 }
