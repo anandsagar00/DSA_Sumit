cfc68c34f4b1ae603ee3707ce821cabfe12d4ef7
Anand Sagar <anandsagar152@gmail.com>
2023-01-26 13:42:47 +0530

    TODO : Visit pattern 10 in future

---
+++ pattern10.cpp
@@ -0,0 +1,15 @@
+// https://youtu.be/X90L33S2xk0?list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk
+
+// https://pepvids.sgp1.cdn.digitaloceanspaces.com/articles/pattern_10/pattern_10_1.png
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    
+
+}
