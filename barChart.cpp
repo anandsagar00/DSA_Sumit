88c9b6f8c77a29fa0631dae32c7d0625dca85063
Anand Sagar <anandsagar152@gmail.com>
2023-01-26 14:35:25 +0530

    Added : Pattern for bar chart

---
+++ barChart.cpp
@@ -0,0 +1,42 @@
+// https://youtu.be/85gIj4eBdBA?list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk
+
+#include<iostream>
+using namespace std;
+
+int find_max(int arr[],int n)
+{
+    int ans=arr[0];
+    for(int i=0;i<n;i++)
+    ans=max(ans,arr[i]);
+
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+
+    int mx=find_max(arr,n);
+
+    int cols=n;
+    int rows=mx;
+
+    for(int i=rows;i>=1;i--)
+    {
+        for(int j=0;j<cols;j++)
+        {
+            if(arr[j]==i)
+            {
+                cout<<"**\t";
+                arr[j]--;
+            }
+            else
+            cout<<" \t";
+        }
+        cout<<"\n";
+    }
+}
