334e10ab04e9e2169d413346d4e3787550cadaa2
Anand Sagar <anandsagar152@gmail.com>
2023-01-30 12:34:40 +0530

    Added : Saddle point in a matrix,min in row and max in col

--- saddle_pt_of_Matrix.cpp
+++ saddle_pt_of_Matrix.cpp
@@ -2,6 +2,14 @@
 
 // Solution : https://youtu.be/6YIWq2JGzEQ?list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk
 
+/*
+The saddle point is defined as the least value in the row and the maximum value in the column.
+
+//Saddle point reference image : https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Saddle_point.svg/1200px-Saddle_point.svg.png
+*/
+
+// Note : There can only be 1 or 0 saddle point in a matrix , think why 
+
 #include <iostream>
 #include<algorithm>
 #include <vector>
@@ -39,9 +47,41 @@ void printMatrix(vector<vector<int> > &arr1)
 
 void findSaddlePt(vector<vector<int> > arr)
 {
-    int r1 = arr1.size();
-    int c1 = arr1[0].size();
-    
+    int r = arr.size();
+    int c = arr[0].size();
+
+    int flag=0;//this will denote if saddle point found or not
+    //Logic : find the min in the row and check if it is max in that col .
+
+    for(int i=0;i<r;i++)
+    {
+        int min_col=0;
+        int mn=arr[i][0];
+        //finding the min in the row
+        for(int j=1;j<c;j++)
+        {
+            if(arr[i][j]<mn)
+            {
+                mn=arr[i][j];
+                min_col=j;
+            }
+        }
+        //finding max in the col
+        int mx=arr[0][min_col];
+        for(int j=1;j<r;j++)
+        {
+            mx=max(mx,arr[j][min_col]);
+        }
+        if(mn==mx)
+        {
+            cout<<"\nSaddle point is : "<<mx<<"\n";
+            flag=1;
+            break;
+        }
+    }
+
+    if(flag==0)
+    cout<<"\nNO SADDLE POINT.\n";
 }
 
 int main()
